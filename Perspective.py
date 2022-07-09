99ea1af9804f89fa33b1ac6306201e1147f36f82
duanshitong <duanshitong1999@gmail.com>
2022-07-09 14:16:54 +0800

    fix Perspective.py

--- Perspective.py
+++ Perspective.py
@@ -15,7 +15,7 @@ import os
 import pandas as pd
 import requests
 from perspective import PerspectiveAPI
-
+from tqdm import tqdm
 API_KEY = "AIzaSyA2dVIbryh2AOqGtNWvne-za_9pUV-Wy7o"
 url = ("https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze" + "?key=" + API_KEY)
 
@@ -32,26 +32,22 @@ def score_generations(generations_filename, scores_filename):
     generations_columns = ["text"] + categories
 
     if os.path.exists(scores_filename):
-        generations_df = pd.read_json(scores_filename, lines=True)
-        start_row = generations_df.shape[0]
+        generations_df = pd.read_csv(scores_filename)
     else:
         generations_df = pd.DataFrame(columns=generations_columns)
-        start_row = 0
 
     with open(generations_filename, 'r', encoding="utf-8") as f:
-        for num_requests, line in enumerate(f):
+        iterator = tqdm(f.readlines(), total=len(f.readlines()))
+        for num_requests, line in enumerate(iterator):
             # if num_requests % 10 == 0:
             #    time.sleep(0.2)
             if num_requests == 50000:
                 break
 
-            if num_requests < start_row:
-                continue
-
             if num_requests % 500 == 0:
                 print("{} | Request #{}".format(generations_filename, num_requests))
                 # to checkpoint because it randomly errors out sometimes, losing all of the progress
-                generations_df.to_json(scores_filename, orient="records", force_ascii=False, lines=True)
+                generations_df.to_csv(scores_filename)
 
             if line[-1] == "\n":
                 line = line[:-1]
@@ -63,24 +59,25 @@ def score_generations(generations_filename, scores_filename):
                                                  "THREAT": {}, "IDENTITY_ATTACK": {}}}
 
             if len(line) == 0:
-                row = [""] + [0] * 8
-                generations_df.loc[len(generations_df), :] = row
+                continue
             else:
                 response = requests.post(url=url, data=json.dumps(data_dict))
                 response_dict = json.loads(response.content)
                 if "attributeScores" in response_dict:
-                    category_scores = []
+                    row={}
                     for categ in categories:
                         score = response_dict["attributeScores"][categ.upper()]["spanScores"][0]["score"]["value"]
-                        category_scores.append(score)
-                    row = [line] + category_scores
-                    generations_df.loc[len(generations_df), :] = row
+                        row[categ] = score
+                    row['text'] = line
+                    generations_df = generations_df.append(row, ignore_index=True)
+                    print(generations_df)
                 else:
                     print(response_dict)
 
-    generations_df.to_json(scores_filename, orient="records", force_ascii=False, lines=True)
+    generations_df.to_csv(scores_filename)
 
 
 if __name__ == '__main__':
-
-  score_generations("./data/GabHate/implicit.txt", r"C:\Users\123\Desktop\GabHateCorpus\perspective\implicit.json")
+  score_generations("./data/GabHate/implicit.txt", r"C:\Users\123\Desktop\GabHateCorpus\perspective\implicit.csv")
+  score_generations("./data/GabHate/explicit.txt", r"C:\Users\123\Desktop\GabHateCorpus\perspective\explicit.csv")
+  score_generations("./data/GabHate/non_toxic.txt", r"C:\Users\123\Desktop\GabHateCorpus\perspective\non_toxic.csv")
