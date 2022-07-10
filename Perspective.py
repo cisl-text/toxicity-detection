015be5618b54745b0ac324f944649ba7240f924c
duanshitong <duanshitong1999@gmail.com>
2022-07-10 17:20:04 +0800

    fix Perspective.py

--- Perspective.py
+++ Perspective.py
@@ -11,6 +11,7 @@
 '''
 import json
 import os
+import time
 
 import pandas as pd
 import requests
@@ -27,28 +28,43 @@ def get_toxicity(text):
 
 
 def score_generations(generations_filename, scores_filename):
+    # csv columns
     categories = ["toxicity", "severe_toxicity", "insult", "flirtation", \
                   "profanity", "sexually_explicit", "threat", "identity_attack"]
     generations_columns = ["text"] + categories
-
+    resume_mode = False
     if os.path.exists(scores_filename):
         generations_df = pd.read_csv(scores_filename)
+        resume_mode = True
     else:
         generations_df = pd.DataFrame(columns=generations_columns)
+        generations_df.to_csv(scores_filename, index=False)
 
     num_requests = 0
     with open(generations_filename, 'r', encoding="utf-8") as f:
-        iterator = f.readlines()
-        for line in tqdm(iterator, total=len(iterator)):
-            # if num_requests % 10 == 0:
-            #    time.sleep(0.2)
+        all_texts = f.readlines()
+        # resume
+        if resume_mode:
+            # 保存的最后一个
+            last_text = generations_df.text.values[-1]
+            # try to find last text
+            start_idx = all_texts.index(last_text + '\n') + 1
+            assert  start_idx < len(all_texts)
+            all_texts = all_texts[start_idx+1:]
+            print("resume mode activated. starting from:", start_idx)
+            generations_df = pd.DataFrame(columns=generations_columns) # 内存中df清空
+
+        for line in tqdm(all_texts, total=len(all_texts)):
+            if num_requests % 10 == 0:
+               time.sleep(0.2)
             if num_requests == 50000:
                 break
 
             if num_requests % 500 == 0:
                 print("{} | Request #{}".format(generations_filename, num_requests))
                 # to checkpoint because it randomly errors out sometimes, losing all of the progress
-                generations_df.to_csv(scores_filename)
+                generations_df.to_csv(scores_filename, mode='a', header=False, index=False)
+                generations_df = pd.DataFrame(columns=generations_columns) # 内存中df清空
 
             if line[-1] == "\n":
                 line = line[:-1]
@@ -75,10 +91,13 @@ def score_generations(generations_filename, scores_filename):
                 else:
                     print(response_dict)
 
-    generations_df.to_csv(scores_filename)
+    generations_df.to_csv(scores_filename, mode='a', header=False, index=False)
 
 
 if __name__ == '__main__':
-  score_generations("./data/GabHate/implicit.txt", r"C:\Users\123\Desktop\GabHateCorpus\perspective\implicit.csv")
-  score_generations("./data/GabHate/explicit.txt", r"C:\Users\123\Desktop\GabHateCorpus\perspective\explicit.csv")
-  score_generations("./data/GabHate/non_toxic.txt", r"C:\Users\123\Desktop\GabHateCorpus\perspective\non_toxic.csv")
+    #score_generations("./data/GabHate/implicit.txt", r"C:\Users\123\Desktop\GabHateCorpus\perspective\implicit.csv")
+    # score_generations("./data/GabHate/explicit.txt", r"C:\Users\123\Desktop\GabHateCorpus\perspective\explicit.csv")
+    # score_generations("./data/GabHate/non_toxic.txt", r"C:\Users\123\Desktop\GabHateCorpus\perspective\non_toxic.csv")
+    score_generations("./data/ImplicitHate/implicit.txt", r"C:\Users\123\Desktop\implicit-hate-corpus\perspective\implicit.csv")
+    score_generations("./data/ImplicitHate/explicit.txt", r"C:\Users\123\Desktop\implicit-hate-corpus\perspective\explicit.csv")
+    score_generations("./data/ImplicitHate/non_toxic.txt", r"C:\Users\123\Desktop\implicit-hate-corpus\perspective\non_toxic.csv")
